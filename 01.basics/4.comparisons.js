143b2cd8799e9f7445fdcf1b6367af319758ab39
Ankit Kumar <86267078+iamankit080@users.noreply.github.com>
2024-07-13 07:42:15 +0000

    comparison of operator

---
+++ 01.basics/4.comparisons.js
@@ -0,0 +1,8 @@
+console.log(null==0);
+console.log(null>=0);
+console.log(null>0);
+
+// compariosn operator convert null into 0.
+//note these conversion are avoided, because of confusion.
+
+// strict check(===)--check value and datatype.
